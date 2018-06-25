b868510f5ed227ff239d233869a634fe49d5b678
YoshiDesign <yoshidesigns@outlook.com>
2018-06-25 13:18:38 -0400

    watch

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -1,6 +1,13 @@
+function escapeKeyListener(evt){
+	if(evt.keyCode === 27 && app.modalOpen){
+		app.modalOpen = false;
+	}
+}
+
 var app = new Vue({
 	el:"#app",
-	data : {
+	data : {						// basic reactive instances
+		bugHook : "RUNNING",
 		title: sample.title,
 		address: sample.address,
 		about: sample.about,
@@ -11,6 +18,26 @@ var app = new Vue({
 		prices: sample.prices,
 		contracted: true,
 		modalOpen: false
+	},
+	
+	watch : {						// Reactivity
+		modalOpen : function(){
+			var className = 'modal-open';
+			if (this.modalOpen){
+				document.body.classList.add(className);
+			} else {
+				document.body.classList.remove(className)
+			}
+		}
+	},
+
+	beforeCreate:function(){
+		console.log("Before: " + this.bugHook); // undef
+	},
+	created: function(){
+		console.log("After: "+ this.bugHook);	// def
+		document.addEventListener('keyup', escapeKeyListener);
 	}
+
 });
 
